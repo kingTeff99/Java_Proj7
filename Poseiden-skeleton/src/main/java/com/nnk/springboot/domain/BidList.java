e403d51bc1228167e23443da37bebd36f5147d17
Teff MAKONDE <Teffmakonde@gmail.com>
2021-08-22 12:37:50 +0200

    solve problem on pom.xml file

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -2,14 +2,63 @@ package com.nnk.springboot.domain;
 
 import org.springframework.beans.factory.annotation.Required;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import java.sql.Date;
 import java.sql.Timestamp;
 
+@Data@AllArgsConstructor@NoArgsConstructor
 @Entity
 @Table(name = "bidlist")
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+	
+	private Integer bidListId ;
+	
+	private String account ;
+	
+	private String type ;
+	
+	private Double bidQuantity ;
+	
+	private Double askQuantity ;
+	
+	private Double bid ;
+	
+	private Double ask ;
+	
+	private String benchmark ;
+	
+	private Timestamp bidListDate ;
+	
+	private String commentary ;
+	
+	private String security ;
+	
+	private String status ;
+	
+	private String trader ;
+	
+	private String book ;
+	
+	private String creationName ;
+	
+	private Timestamp creationDate ;
+	
+	private String revisionName ;
+	
+	private Timestamp revisionDate ;
+	
+	private String dealName ;
+	
+	private String dealType ;
+	
+	private String sourceListId ;
+	
+	private String side ;
+	
 }
